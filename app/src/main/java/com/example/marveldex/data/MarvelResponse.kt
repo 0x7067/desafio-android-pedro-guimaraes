be372bdee3913b7a3f395b3d6aca51dfc7b0105d
Pedro Guimarares <moisespedro15@gmail.com>
2020-03-10 11:30:27 -0300

    Remove unused fields

--- app/src/main/java/com/example/marveldex/data/MarvelResponse.kt
+++ app/src/main/java/com/example/marveldex/data/MarvelResponse.kt
@@ -4,11 +4,7 @@ import com.google.gson.annotations.SerializedName
 
 open class MarvelResponse(
     @SerializedName("code") protected var code: Int = 0,
-    @SerializedName("status") protected var status: String = "",
-    @SerializedName("copyright") protected var copyright: String = "",
-    @SerializedName("attributionText") protected var attributionText: String = "",
-    @SerializedName("attributionHTML") protected var attributionHTML: String = "",
-    @SerializedName("etag") protected var etag: String = ""
+    @SerializedName("status") protected var status: String = ""
 ) : IMarvelResponse {
     override fun isSuccess(): Boolean {
         return code == 200
