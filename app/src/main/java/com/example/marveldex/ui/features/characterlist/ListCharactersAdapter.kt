f4f3764911fb454d90ccf5479bf1763587bfb0c9
Pedro Guimarares <moisespedro15@gmail.com>
2020-03-11 10:26:55 -0300

    Show hero's image

---
+++ app/src/main/java/com/example/marveldex/ui/features/characterlist/ListCharactersAdapter.kt
@@ -0,0 +1,47 @@
+package com.example.marveldex.ui.features.characterlist
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import coil.api.load
+import com.example.marveldex.R
+import com.example.marveldex.data.MarvelHero
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.item_character.view.*
+
+class ListCharactersAdapter(private val heroList: MutableList<MarvelHero>, private val onHeroClicked: () -> Unit) : RecyclerView.Adapter<ListCharactersAdapter.ItemViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        return ItemViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_character,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val hero = heroList[position]
+        holder.itemView.characterName.text = hero.name
+        val heroUrl = hero.thumbnail.path.replace("http", "https") + "." + hero.thumbnail.extension
+
+        holder.itemView.characterImage.load(heroUrl)
+
+        holder.itemView.setOnClickListener { onHeroClicked() }
+    }
+
+    fun setHeroesList(heroList: List<MarvelHero>) {
+        this.heroList.clear()
+        this.heroList.addAll(heroList)
+        notifyDataSetChanged()
+    }
+
+    class ItemViewHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView),
+        LayoutContainer
+
+    override fun getItemCount(): Int {
+        return heroList.size
+    }
+}
--- app/src/main/java/com/example/marveldex/ui/features/characterlist/ListCharactersFragment.kt
+++ app/src/main/java/com/example/marveldex/ui/features/characterlist/ListCharactersFragment.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.marveldex.R
+import com.example.marveldex.data.MarvelHero
+import kotlinx.android.synthetic.main.list_characters_fragment.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.cancel
@@ -27,7 +31,10 @@ class ListCharactersFragment : Fragment(), CoroutineScope by MainScope() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(ListCharactersViewModel::class.java)
         launch {
-            viewModel.fetchMarvelHeroes()
+            val heroes = viewModel.fetchMarvelHeroes()
+            val adapter = ListCharactersAdapter(heroes as MutableList<MarvelHero>) {Toast.makeText(requireContext(), "oi", Toast.LENGTH_LONG).show() }
+            charactersRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+            charactersRecyclerView.adapter = adapter
         }
     }
 
