2af2899d63b46b14d492144053cb47d6e284b448
Pedro Guimarares <moisespedro15@gmail.com>
2020-03-11 13:52:03 -0300

    Show comics details

--- app/src/main/java/com/example/marveldex/ui/features/characterdetails/CharacterDetailsViewModel.kt
+++ app/src/main/java/com/example/marveldex/ui/features/characterdetails/CharacterDetailsViewModel.kt
@@ -2,10 +2,12 @@ package com.example.marveldex.ui.features.characterdetails
 
 import androidx.lifecycle.ViewModel
 import com.example.marveldex.api.MarvelClient
+import com.example.marveldex.data.comics.MarvelComics
 
 class CharacterDetailsViewModel() : ViewModel() {
 
-    suspend fun fetchHeroComics(heroID: Int) {
+    suspend fun fetchHeroComics(heroID: Int): List<MarvelComics> {
         val heroComics = MarvelClient.getComicsByHeroId(heroID)
+        return heroComics.heroData.results
     }
 }
