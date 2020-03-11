ce49f515f870cb5d8a7f8223b7a1e2f74327dd4f
Pedro Guimarares <moisespedro15@gmail.com>
2020-03-11 10:26:55 -0300

    Show hero's image

--- app/src/main/java/com/example/marveldex/ui/features/characterlist/ListCharactersViewModel.kt
+++ app/src/main/java/com/example/marveldex/ui/features/characterlist/ListCharactersViewModel.kt
@@ -2,11 +2,12 @@ package com.example.marveldex.ui.features.characterlist
 
 import androidx.lifecycle.ViewModel
 import com.example.marveldex.api.MarvelClient
+import com.example.marveldex.data.MarvelHero
 
 class ListCharactersViewModel() : ViewModel() {
 
-    suspend fun fetchMarvelHeroes() {
+    suspend fun fetchMarvelHeroes() : List<MarvelHero> {
         val heroes = MarvelClient.getMarvelHeroes(20, 0)
-        print(heroes.heroData.count)
+        return heroes.heroData.results
     }
 }
